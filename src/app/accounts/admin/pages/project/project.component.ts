1178efff25b2877d47f810fb66f292ed62b6ed23
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-19 13:31:58 +0000

    project component

--- src/app/accounts/admin/pages/project/project.component.ts
+++ src/app/accounts/admin/pages/project/project.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonNewComponent } from '../../../user/components/button-new/button-new.component';
+import { ProjectCreationModalComponent } from '../../../../shared/components/modals/project-creation-modal/project-creation-modal.component';
+import { ProjectTableComponent } from '../../components/project-table/project-table.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonNewComponent,
+    ProjectCreationModalComponent,
+    ProjectTableComponent
+  ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
+  projectCreationModalOpen = false;
+
+  openProjectCreationModal() {
+    this.projectCreationModalOpen = true;
+  }
 
 }

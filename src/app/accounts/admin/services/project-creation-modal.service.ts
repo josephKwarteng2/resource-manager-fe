f9135238eb756b0a2dd770922c3d451ae88a1dd9
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-19 13:33:01 +0000

    project creation services

---
+++ src/app/accounts/admin/services/project-creation-modal.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { BASE_URL } from '../../../../environment/config';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectDetails } from '../../../shared/types/types';
+import { ProjectCreationModalComponent } from '../../../shared/components/modals/project-creation-modal/project-creation-modal.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectCreationModalService {
+  constructor(private http: HttpClient, private projectcreationmodalService: NgbModal) {}
+  addNewProject(data: ProjectDetails): Observable<ProjectDetails> {
+    return this.http.post<ProjectDetails>(`${BASE_URL}/project/store`, data);
+  }
+
+getProjects(): Observable<ProjectDetails[]> {
+  return this.http.get<ProjectDetails[]>(`${BASE_URL}/project/fetch`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': 'skip-browser-warning',
+    },
+  })
+  .pipe(
+    catchError(error => {
+      console.error('Error in getClients:', error);
+      throw error; // rethrow the error
+    })
+  );
+}
+
+  openProjectCreationModal(): NgbModalRef {
+
+    const modalRef = this.projectcreationmodalService.open(ProjectCreationModalComponent, {
+      centered: true,
+      backdrop: 'static', 
+
+    });
+    
+    modalRef.result.finally(() => {
+     
+    });
+
+    return modalRef;
+
+
+  }
+
+}

fe0b2c6052b3c26f9ad6eeeb90779fc150004286
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-19 13:31:32 +0000

    project table component

---
+++ src/app/accounts/admin/components/project-table/project-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,} from '@angular/core';
+import { ClientDetails, GenericResponse } from '../../../../shared/types/types';
+import { ClientCreationModalService } from '../../services/client-creation-modal.service';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-project-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-table.component.html',
+  styleUrl: './project-table.component.css'
+})
+export class ProjectTableComponent {
+
+}

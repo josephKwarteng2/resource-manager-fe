6565e68f3b0b0dc69b9fb04511467621240c7259
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-19 13:33:25 +0000

    client component

--- src/app/accounts/admin/pages/client/client.component.ts
+++ src/app/accounts/admin/pages/client/client.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ButtonNewComponent } from '../../../user/components/button-new/button-new.component';
+import { ClientCreationModalComponent } from '../../../../shared/components/modals/client-creation-modal/client-creation-modal.component';
+import { ClientTableComponent } from '../../components/client-table/client-table.component';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonNewComponent,
+    ClientCreationModalComponent,
+    ClientTableComponent
+  ],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css', 
 })
 export class ClientComponent {
+  clientCreationModalOpen = false;
+
+  openClientCreationModal() {
+    this.clientCreationModalOpen = true;
+  }
+
 
 }

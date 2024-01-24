68866838f05484163b01cb54acdc2c3c060f7159
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-24 15:30:07 +0000

    cleaned, added pagination and a method to open the view modal

--- src/app/accounts/admin/components/client-table/client-table.component.ts
+++ src/app/accounts/admin/components/client-table/client-table.component.ts
@@ -2,33 +2,54 @@ import { Component, OnInit,} from '@angular/core';
 import { ClientDetails, GenericResponse } from '../../../../shared/types/types';
 import { ClientCreationModalService } from '../../services/client-creation-modal.service';
 import { CommonModule } from '@angular/common';
+import { ClientDetailsComponent } from '../../../../shared/components/modals/client-details/client-details.component';
+import { PaginationComponent } from '../../../../shared/components/pagination/pagination.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-client-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClientDetailsComponent, PaginationComponent],
   templateUrl: './client-table.component.html',
   styleUrl: './client-table.component.css'
 })
 export class ClientTableComponent implements OnInit {
+  totalPages: number = 0;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
   clients : ClientDetails[] = [];
   loading: boolean = false;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+  totalClients: number = 0;
+
   constructor(
-   private clientcreationmodalService: ClientCreationModalService,){}
+   private clientcreationmodalService: ClientCreationModalService,private modalService: NgbModal){}
   ngOnInit(): void {
     this.loading = false;
    this.fetchClients();
   }
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.fetchClients();
+  }
+
 
   fetchClients(): void {
     this.loading = true;
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+
+    const endIndex = startIndex + this.itemsPerPage;
+
     this.clientcreationmodalService.getClients().subscribe(
       (response: any) => {
-        console.log(response);
+       
         const clients = response.clients || response;
         if (Array.isArray(clients)) {
-          this.clients = clients as ClientDetails[];
+          this.clients = clients.slice(startIndex, endIndex) as ClientDetails[];
+          this.totalClients = clients.length;
+          this.totalPages = Math.ceil(clients.length / this.itemsPerPage);
         } else {
           console.error('Invalid response format for clients:', clients);
         }
@@ -41,6 +62,9 @@ export class ClientTableComponent implements OnInit {
       }
     );
   }
-  
+  openClientsDetails(client: ClientDetails): void {
+    const modalRef = this.modalService.open(ClientDetailsComponent);
+    modalRef.componentInstance.client = client;
+  }
 
 }

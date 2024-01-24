1e173bff0ac3e10027f4d6e685df2dff9166358f
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-24 15:39:34 +0000

    made changes to clean the code

--- src/app/accounts/admin/services/project-creation-modal.service.ts
+++ src/app/accounts/admin/services/project-creation-modal.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { BASE_URL } from '../../../../environment/config';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectDetails, GenericResponse } from '../../../shared/types/types';
 import { ProjectCreationModalComponent } from '../../../shared/components/modals/project-creation-modal/project-creation-modal.component';
+import { ErrorHandlingService } from './error-handling.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectCreationModalService {
-  constructor(private http: HttpClient, private projectcreationmodalService: NgbModal) { }
+  constructor(
+    private http: HttpClient,
+    private projectCreationModalService: NgbModal,
+    private errorHandlingService: ErrorHandlingService // Inject the error handling service
+  ) { }
+
   addNewProject(data: ProjectDetails): Observable<ProjectDetails> {
-    return this.http.post<ProjectDetails>(`${BASE_URL}/project/store`, data);
+    return this.http.post<ProjectDetails>(`${BASE_URL}/project/store`, data).pipe(
+      catchError((error: HttpErrorResponse) => this.handleError(error))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      this.errorHandlingService.handleError('Client-side error: ' + error.error.message);
+    } else {
+      this.errorHandlingService.handleError(`Server returned code ${error.status}, body was: ${error.error}`);
+    }
+
+    return throwError(error);
   }
 
   getProjects(): Observable<ProjectDetails[]> {
@@ -24,11 +42,12 @@ export class ProjectCreationModalService {
     })
       .pipe(
         catchError(error => {
-          console.error('Error in getClients:', error);
-          throw error; // rethrow the error
+          this.errorHandlingService.handleError('Error in getClients: ' + error);
+          throw error;
         })
       );
   }
+
   deleteProject(projectCode: string): Observable<GenericResponse> {
     return this.http.delete<GenericResponse>(
       `${BASE_URL}/projects/delete`,
@@ -45,20 +64,15 @@ export class ProjectCreationModalService {
   }
 
   openProjectCreationModal(): NgbModalRef {
-
-    const modalRef = this.projectcreationmodalService.open(ProjectCreationModalComponent, {
+    const modalRef = this.projectCreationModalService.open(ProjectCreationModalComponent, {
       centered: true,
       backdrop: 'static',
-
     });
 
     modalRef.result.finally(() => {
-
+      // You can handle any final tasks here
     });
 
     return modalRef;
-
-
   }
-
 }

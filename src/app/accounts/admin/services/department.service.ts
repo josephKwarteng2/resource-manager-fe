e54a90ebf46497ce8a6d57a691999a27e038f347
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-24 15:38:33 +0000

    made changes to clean the code

--- src/app/accounts/admin/services/department.service.ts
+++ src/app/accounts/admin/services/department.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { BASE_URL } from '../../../../environment/config';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { DepartmentModalComponent } from '../../../shared/components/modals/department-modal/department-modal.component';
+import { GenericResponse } from '../../../shared/types/types';
 
 @Injectable({
   providedIn: 'root',
@@ -37,18 +38,15 @@ export class DepartmentService {
 
   }
 
-  addDepartment(department: string): Observable<any> {
+  addDepartment(department: string): Observable<GenericResponse> {
     const currentDepartments = this._departments.getValue();
-    // const updatedDepartments = [...currentDepartments, department];
-    // console.log('Updated Departments:', updatedDepartments);
-    // this._departments.next(updatedDepartments);
+ 
 
     return this.http
       .post<any>(`${BASE_URL}/department/store`, { name: department })
       .pipe(
         catchError(error => {
-          console.error('Error adding department to the backend:', error);
-          this._departments.next(currentDepartments);
+         this._departments.next(currentDepartments);
           return throwError(error);
         })
       );
@@ -64,8 +62,7 @@ export class DepartmentService {
       })
       .pipe(
         catchError(error => {
-          console.error('Error fetching departments:', error);
-          return throwError(error);
+         return throwError(error);
         })
       );
   }

f5d7a603c40d7ae84331d9e0fbee92d0550eb42f
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-24 15:40:50 +0000

    method to open a create client modal

--- src/app/accounts/manager/pages/client/client.component.ts
+++ src/app/accounts/manager/pages/client/client.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonNewComponent } from '../../../user/components/button-new/button-new.component';
+import { ClientCreationModalComponent } from '../../../../shared/components/modals/client-creation-modal/client-creation-modal.component';
+import { ClientTableComponent } from '../../../admin/components/client-table/client-table.component';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonNewComponent, ClientCreationModalComponent, ClientTableComponent, 
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
+  clientCreationModalOpen = false;
+
+  openClientCreationModal() {
+    this.clientCreationModalOpen = true;
+  }
+
+
 
 }

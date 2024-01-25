b9e5b3b818cd5ee4b859da0500414e670d537bfd
JosephKWarteng1 <joseph.kwarteng@amalitech.com>
2024-01-25 20:33:58 +0100

    minor change

--- src/app/shared/types/types.ts
+++ src/app/shared/types/types.ts
@@ -29,6 +29,9 @@ export type CurrentUser = {
   bookable: boolean;
   created_at: string;
   selected?: boolean;
+  timeZone: string;
+  client: string;
+  project: string;
 };
 
 export type AdminUser = Pick<CurrentUser, 'email' | 'department' | 'roles'> & {
